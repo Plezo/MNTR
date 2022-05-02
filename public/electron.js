d05ff0b92a3ffe4cfb9cd9e61edc3a157b73cd5f
Patryk Lezon <Plezo@protonmail.com>
2022-05-01 23:09:21 -0400

    Started working on settings (not working)

--- public/electron.js
+++ public/electron.js
@@ -9,14 +9,15 @@ function createWindow() {
     width: 1200,
     height: 800,
     title: "MNTR",
-    resizeable: false,
     // frame: false, // you're gonna wanna make your own frame to make it look nicer
     webPreferences: {
       nodeIntegration: true,
+      contextIsolation: false,
     },
   });
 
   win.setMenuBarVisibility(false);
+  win.setResizable(false);
 
   // and load the index.html of the app.
   // win.loadFile("index.html");
--- src/pages/MintPage.js
+++ src/pages/MintPage.js
@@ -7,7 +7,11 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './MintPage.css'
 
+const { app } = require('electron');
+const fs = window.require('fs');
+
 export default function MintPage() {
+  const [profileName, setProfileName] = useState('');
   const [privateKey, setPrivateKey] = useState('');
   const [RPCURL, setRPCURL] = useState('');
   const [contractAddress, setContractAddress] = useState('');
@@ -15,9 +19,25 @@ export default function MintPage() {
   const [price, setPrice] = useState(0);
   const [amount, setAmount] = useState(0);
 
+  // eventually change this into just getting it from ABI 
+  // and having a dropdown for choosing the function
+  const [functionName, setFunctionName] = useState('');
+  const [parameters, setParameters] = useState([]);
+
   // might be useful?
   function isValidPrivateKey() {
     return ethers.utils.isHexString(privateKey);
+  }
+
+  function formatParams(paramArr) {
+    let returnStr = "";
+    for (let i = 0; i < paramArr.length; i++) {
+      if (paramArr[i] === 'uint') paramArr[i] = 'uint256';
+
+      returnStr += `${paramArr[i]}, `;
+    }
+
+    return returnStr.slice(0, -2);
   }
 
   // nothing in this func is useful rn
@@ -46,9 +66,35 @@ export default function MintPage() {
     const Wallet = new ethers.Wallet(privateKey, Provider);
     const Contract = new ethers.Contract(contractAddress, contractABI, Wallet);
 
-    // figure out how to call a function by name
-    const tx = await Contract.publicMint(amount, {value: ethers.utils.parseUnits(`${price*amount}`, "ether")});
-    console.log(tx)
+    const params = [amount, {value: ethers.utils.parseUnits(`${price*amount}`, "ether")}]
+    await Contract[`${functionName}(${formatParams(parameters)})`](...params);
+
+    e.preventDefault();
+  }
+
+  // have a popup asking for profile name
+  // have a popup for success
+  const saveButtonEvent = async (e) => {
+    
+    let toSave = {
+      profileName: profileName,
+      privateKey: privateKey,
+      RPCURL: RPCURL,
+      contractAddress: contractAddress,
+      contractABI: contractABI,
+      price: price,
+      amount: amount,
+      functionName: functionName,
+      parameters: parameters
+    }
+
+    fs.writeFileSync(`${profileName}.json`, JSON.stringify(toSave, null, 2));
+
+    e.preventDefault();
+  }
+
+  const loadButtonEvent = async (e) => {
+    
 
     e.preventDefault();
   }
@@ -94,7 +140,9 @@ export default function MintPage() {
               </Form.Floating>
             </Form>
           </Col>
+        </Row>
 
+        <Row>
           <Col md="2">
             <Form className="PriceForm" onSubmit={onSubmit}>
               <Form.Floating className="mb-3">
@@ -112,13 +160,50 @@ export default function MintPage() {
               </Form.Floating>
             </Form>
           </Col>
+
+          <Col md="2">
+            <Form className="FunctionForm" onSubmit={onSubmit}>
+              <Form.Floating className="mb-3">
+                  <Form.Control id="floatingInputCustom" type="text" placeholder="mint" onChange={(e) => {setFunctionName(e.target.value)}}/>
+                  <Form.Label htmlFor="floatingInputCustom">Function</Form.Label>
+              </Form.Floating>
+            </Form>
+          </Col>
+
+          <Col md="2">
+            <Form className="ParamsForm" onSubmit={onSubmit}>
+              <Form.Floating className="mb-3">
+                  <Form.Control id="floatingInputCustom" type="text" placeholder="uint256" onChange={(e) => {setParameters(e.target.value.split(' '))}}/>
+                  <Form.Label htmlFor="floatingInputCustom">Params</Form.Label>
+              </Form.Floating>
+            </Form>
+          </Col>
+
+          <Col md="2">
+            <Form className="ProfileNameForm" onSubmit={onSubmit}>
+              <Form.Floating className="mb-3">
+                  <Form.Control id="floatingInputCustom" type="text" placeholder="profile" onChange={(e) => {setProfileName(e.target.value)}}/>
+                  <Form.Label htmlFor="floatingInputCustom">Profile Name</Form.Label>
+              </Form.Floating>
+            </Form>
+          </Col>
         </Row>
       </Container>
 
         <Row className="justify-content-md-center">
           <Col md="auto">
-            <Button variant="primary" size="lg" onClick={mintButtonEvent}>
+            <Button variant="success" size="lg" onClick={mintButtonEvent}>
               Mint
+            </Button>
+          </Col>
+          <Col md="auto">
+            <Button variant="primary" size="lg" onClick={saveButtonEvent}>
+              Save Profile
+            </Button>
+          </Col>
+          <Col md="auto">
+            <Button variant="primary" size="lg" onClick={loadButtonEvent}>
+              Load Profile
             </Button>
           </Col>
         </Row>
